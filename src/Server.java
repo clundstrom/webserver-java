3cdae5e70d708e045ac5455cdc3f321fc53dd926
Christoffer <lulleh@mail.clundstrom.com>
2020-01-29 21:06:10 +0100

    fixed server

--- src/Server.java
+++ src/Server.java
@@ -1,28 +1,45 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         // Create server socket and bind to port.
-        ServerSocket socket = new ServerSocket(6000);
-
+        ServerSocket serverSocket = new ServerSocket(6000);
+        byte[] buff = new byte[1024];
 
 
         while(true){
 
-            Socket incoming = socket.accept();
+            Socket incoming = serverSocket.accept();
             System.out.println("Creating thread for incoming request: " + incoming.getInetAddress() + " " + incoming.getPort());
 
-            // Transer input stream to output stream
-            incoming.getInputStream().transferTo(incoming.getOutputStream());
+            InputStream in = incoming.getInputStream();
+            int read;
+
+            StringBuilder sr = new StringBuilder();
+            while((read = in.read(buff)) != -1){
+                sr.append(new String(buff, 0, read));
+                read = in.read(buff);
+            }
+            incoming.shutdownInput();
+
+            System.out.println("Responding to client: ");
+            System.out.println(sr.toString());
+            System.out.println("End of stream.");
 
 
+            PrintWriter pw = new PrintWriter(incoming.getOutputStream());
+            pw.println(sr.toString());
+
+
+            pw.close();
+            in.close();
+            incoming.close();
         }
-
-
-
     }
 }
