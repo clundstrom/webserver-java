a8686e42c062920ac0a8692019595f0658f23036
Christoffer Lundström <lulleh_@hotmail.com>
2020-01-30 10:19:38 +0100

    1

--- src/TCPTransmitTask.java
+++ src/TCPTransmitTask.java
@@ -26,42 +26,16 @@ public class TCPTransmitTask implements Runnable {
     @Override
     public void run() {
         long start = System.currentTimeMillis();
-        long total;
+        long total = processMessages(start);
+
+
+
+
         try {
-            byte[] buf = new byte[buffSize];
-            // Create a Writer to the output-stream
-            PrintWriter output = new PrintWriter(socket.getOutputStream());
-            BufferedReader bufferedReader = null;
-            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            // Process messages
-            for (int i = 0; i < nrOfPackets; i++) {
-
-                // Write to output
-                output.println(message);
-                logger.setSent(logger.getSent()+1);
-
-                int bytesRead = 0;
-               // check input
-                String str;
-                logger.setTotalReceived(logger.getTotalReceived() + 1);
-
-                // Keep track of time spent
-                total = System.currentTimeMillis() - start;
-
-//                // If the time exceeds 1 second abort immediately
-//                if (total >= 999) {
-//                    logger.setRemaining(nrOfPackets - i - 1);
-//                    System.out.println(logger.toString());
-//                    System.exit(0);
-//                }
-            }
+            receiveMessages();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println(logger);
-        total = System.currentTimeMillis();
 
         /* Wait until the full second has passed before terminating thread*/
         try {
@@ -70,6 +44,54 @@ public class TCPTransmitTask implements Runnable {
             e.printStackTrace();
             Thread.currentThread().interrupt();
         }
+    }
 
+
+    private void receiveMessages() throws IOException {
+        byte[] buff = new byte[buffSize];
+        int read;
+        StringBuilder sr = new StringBuilder();
+        InputStream in = this.socket.getInputStream();
+        while((read = in.read(buff)) != -1){
+            System.out.println((new String(buff, 0, read)));
+            read = in.read(buff);
+        }
+    }
+
+    long processMessages(long start) {
+        long total;
+        try {
+            byte[] buf = new byte[buffSize];
+            // Create a Writer to the output-stream
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            // Process messages
+            for (int i = 0; i < nrOfPackets; i++) {
+
+                // Write to output
+                output.println(message);
+                logger.setSent(logger.getSent() + 1);
+
+                // check input
+                String str;
+                logger.setTotalReceived(logger.getTotalReceived() + 1);
+
+                // Keep track of time spent
+                total = System.currentTimeMillis() - start;
+
+                // If the time exceeds 1 second abort immediately
+                if (total >= 999) {
+                    logger.setRemaining(nrOfPackets - i - 1);
+                    System.out.println(logger.toString());
+                    System.exit(0);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(logger);
+        total = System.currentTimeMillis();
+        return total;
     }
 }
