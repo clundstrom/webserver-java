a297bb8eeee81c35cc1b90fcce79a8fd8c3e2544
Christoffer Lundström <lulleh_@hotmail.com>
2020-01-31 17:09:36 +0100

    vg2

---
+++ src/AbstractNetworkLayer.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public abstract class AbstractNetworkLayer {
+
+
+    public AbstractNetworkLayer(){}
+
+    public static int MYPORT;
+    public static String MSG;
+    public static int BUFSIZE;
+    public static int TRANSFER_RATE;
+
+
+
+
+
+
+
+}
